aca4546cab25b39054ffa2b7afe50a09a0b92aea
Hieu <hieus207@gmail.com>
2023-01-12 10:48:17 +0700

    test push

---
+++ single.php
@@ -0,0 +1,121 @@
+<?php include('path.php');?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!--Somewhere font-->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.7.2/css/all.min.css">
+    <!--Google font-->
+    <link href="https://fonts.googleapis.com/css2?family=Candal&family=Lora:ital,wght@1,600&display=swap"
+        rel="stylesheet">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <title>Single Post</title>
+</head>
+
+<body>
+
+    <?php include(ROOT_PATH . "/app/includes/header.php");?>
+
+    <!-- CONTENT   -->
+    <div class="page-wrapper">
+        <div class="content clearfix">
+
+    <!-- maincontent-->
+            <div class="main-content single">
+                <h1 class="post-title">This is title of the website</h1>
+                <div class="post-content">
+                    <p>Every Day is the good day.Hope you have a good day!Good luck! You're welcome</p>
+                    <p>Every Day is the good day.Hope you have a good day!Good luck! You're welcome</p>
+                    <p>Every Day is the good day.You're welcome</p>
+                    <p>Every Day is the good day.You're welcome</p>
+                    <p>You're welcome</p>
+                    <p>Hope you have a good day!Good luck! You're welcome</p>
+                    <p>Every Day is the good day.Hope you have a good day!Good luck!</p>
+                    <p>Every Day is the good day.Hope you have a good day!Good luck! You're welcome</p>
+                    <p>Every Day is the good day.Hope you have a good day!Good luck! You're welcome</p>
+                    
+                    <p>Every Day is the good day.Hope you have a good day!Good luck! You're welcome</p>
+                    <p>Every Day is the good day.Hope you have a good day!Good luck! You're welcome</p>
+                    <p>Every Day is the good day.You're welcome</p>
+                    <p>Every Day is the good day.You're welcome</p>
+                    <p>You're welcome</p>
+                    <p>Hope you have a good day!Good luck! You're welcome</p>
+                    <p>Every Day is the good day.Hope you have a good day!Good luck!</p>
+                    <p>Every Day is the good day.Hope you have a good day!Good luck! You're welcome</p>
+                    <p>Every Day is the good day.Hope you have a good day!Good luck! You're welcome</p>
+                    
+                    <p>Every Day is the good day.Hope you have a good day!Good luck! You're welcome</p>
+                    <p>Every Day is the good day.Hope you have a good day!Good luck! You're welcome</p>
+                    <p>Every Day is the good day.You're welcome</p>
+                    <p>Every Day is the good day.You're welcome</p>
+                    <p>You're welcome</p>
+                    <p>Hope you have a good day!Good luck! You're welcome</p>
+                    <p>Every Day is the good day.Hope you have a good day!Good luck!</p>
+                    <p>Every Day is the good day.Hope you have a good day!Good luck! You're welcome</p>
+                    <p>Every Day is the good day.Hope you have a good day!Good luck! You're welcome</p>
+                    
+                </div>
+            </div>
+    <!--//End Maincontent-->
+    <!--Sidebar-->
+            <div class="sidebar single">
+                <div class="section popular">
+                    <h2 class="section-title">Popular</h2>
+                    <div class="post clearfix">
+                        <img src="assets/image/img1.png" alt="123">
+                        <a href="#" class="title"><h4>How to handsome like HieuTran?</h4></a>
+                    </div>
+                    <div class="post clearfix">
+                        <img src="assets/image/imag2.jpg" alt="123">
+                        <a href="#" class="title"><h4>How to handsome like HieuTran?</h4></a>
+                    </div>
+                    <div class="post clearfix">
+                        <img src="assets/image/img3.png" alt="123">
+                        <a href="#" class="title"><h4>How to handsome like HieuTran?</h4></a>
+                    </div>
+                    <div class="post clearfix">
+                        <img src="assets/image/img4.png" alt="234">
+                        <a href="#" class="title"><h4>How to handsome like HieuTran?</h4></a>
+                    </div>
+                    <div class="post clearfix">
+                        <img src="assets/image/img5.png" alt="345">
+                        <a href="#" class="title"><h4>How to handsome like HieuTran?</h4></a>
+                    </div>
+                </div>
+                    <div class="section topics">
+                        <h2 class="section-title">Topics</h2>
+                        <ul>
+                            <li><a href="index.html">Music</a></li>
+                            <li><a href="index.html">Picture</a></li>
+                            <li><a href="index.html">Relax</a></li>
+                            <li><a href="index.html">Games</a></li>
+                            <li><a href="index.html">Community</a></li>
+                        </ul>
+                    </div>
+
+
+                </div>
+    <!--End - Sidebar-->
+            
+    <!--//End Content-->
+        </div>
+    </div>
+    <!--Footer-->
+    
+    <?php include(ROOT_PATH . "/app/includes/footer.php");?>
+
+    <!--END-Footer-->
+        
+
+        <!-- script SLICKS   -->
+
+        <!--ERROR _ RESP DISPLAY-->
+        <script type="text/javascript"
+            src="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
+        <!-- CUSTOM    -->
+        <script src="assets/js/script.js"></script>
+</body>
+
+</html>
